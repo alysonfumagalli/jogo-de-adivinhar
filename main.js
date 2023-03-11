81bfd2cbdd0cdd1ec1db61436ab06a2927b98955
Alyson César Fumagalli <127056544+alysonfumagalli@users.noreply.github.com>
2023-03-11 18:07:20 -0300

    Realocamento de variáveis e criação de um novo botão
    Realoquei diversas variáveis para torná-las globais e poder utilizá-las em outras funções. Também criei um novo botão "Jogar de novo" para possibilitar que o player jogue novamente.

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-var randomNumber = parseInt(Math.random() * 10) + 1;
+var randomNumber = parseInt(Math.random() * 1000) + 1;
 var tries = 10;
-console.log(randomNumber);
-
 var guessButton = document.getElementById("guessButton");
-guessButton.addEventListener('click', function(){
-    let guessedNumber = document.getElementById("guessedNumber").value;
-    let numbersGuessed = document.getElementById("displayGuessedNumbers");
-    let tryResult = document.getElementById("tryResult");
-    let triesLeft = document.getElementById("triesLeft");
+var playAgainButton = document.getElementById("playAgain");
+var triesLeft = document.getElementById("triesLeft");
+var guessedNumber = document.getElementById("guessedNumber").value;
+var numbersGuessed = document.getElementById("displayGuessedNumbers");
+var tryResult = document.getElementById("tryResult");
 
+guessButton.addEventListener('click', function(){
     if (guessedNumber != randomNumber) {
         numbersGuessed.innerHTML += guessedNumber + " / "
     } else {
@@ -30,6 +29,23 @@ guessButton.addEventListener('click', function(){
 
     if (tries == 0){
         guessButton.disabled = true;
-        tryResult.innerHTML = "Ops...Você perdeu. O número secreto era " + guessedNumber + ".";
+        tryResult.innerHTML = "Ops...Você perdeu. O número secreto era " + randomNumber + ".";
+        if (playAgainButton.style.display == 'none') {
+            playAgainButton.style.display = 'inline';
+        } else {
+            playAgainButton.style.display = 'none';
+        }
     }
+
+    document.getElementById("guessedNumber").value = '';
+})
+
+playAgainButton.addEventListener('click', function(){
+    guessButton.disabled = false;
+    tries = 10;
+    triesLeft.innerHTML = 10;
+    tryResult.innerHTML = '';
+    numbersGuessed.innerHTML = "Números chutados: ";
+    randomNumber = parseInt(Math.random() * 1000) + 1;
+    playAgainButton.style.display = "none";
 })
